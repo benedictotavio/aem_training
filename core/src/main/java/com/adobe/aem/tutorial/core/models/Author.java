aeba62d614926b8ce0c1c181708c9ef39f73a1d4
beneditotavio <otavio.bene@outlook.com>
2024-08-08 01:50:01 -0300

    insert method to insert map payload

--- core/src/main/java/com/adobe/aem/tutorial/core/models/Author.java
+++ core/src/main/java/com/adobe/aem/tutorial/core/models/Author.java
@@ -20,6 +20,9 @@ import com.day.cq.search.QueryBuilder;
 import com.day.cq.wcm.api.Page;
 import org.apache.sling.api.resource.Resource;
 
+import java.util.Map;
+import java.util.HashMap;
+
 @Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class Author {
 
@@ -76,5 +79,26 @@ public class Author {
 
     public String getHomePageName() {
         return resourcePage.getName();
+    }
+
+    public Map<String, String>[] getBookDetailsWithMap() {
+        @SuppressWarnings("unchecked")
+        Map<String, String>[] bookDetailsMap = new Map[3];
+        try {
+            Resource bookDetail = resourcePage.getChild("bookdetailswithmap");
+            if (bookDetail != null) {
+                for (int i = 0; i < 3; i++) {
+                    Resource book = bookDetail.getChild(i + "");
+                    Map<String, String> bookMap = new HashMap<>();
+                    bookMap.put("bookname", book.getValueMap().get("bookname", String.class));
+                    bookMap.put("booksubject", book.getValueMap().get("booksubject", String.class));
+                    bookMap.put("publishyear", book.getValueMap().get("publishyear", String.class));
+                    bookDetailsMap[i++] = bookMap;
+                }
+            }
+        } catch (Exception e) {
+            logger.info("\n ERROR while getting Book Details {} ", e.getMessage());
+        }
+        return bookDetailsMap;
     }
 }
